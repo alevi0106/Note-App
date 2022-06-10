de59197e9802e35a299b880f6f797a497a3fa502
Vaibhav Lanjekar <alevi0106@gmail.com>
2022-06-10 23:18:31 +0530

    Added note card and search button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoteListComponent } from './pages/note-list/note-list.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteCardComponent } from './note-card/note-card.component';
 
 @NgModule({
-  declarations: [AppComponent, NoteListComponent, MainLayoutComponent],
+  declarations: [AppComponent, NoteListComponent, MainLayoutComponent, NoteCardComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/note-list/note-list.component.ts
+++ src/app/pages/note-list/note-list.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-note-list',
   templateUrl: './note-list.component.html',
-  styleUrls: ['./note-list.component.scss']
+  styleUrls: ['./note-list.component.scss'],
 })
 export class NoteListComponent implements OnInit {
+  header = 'This is title';
+  details =
+    'dsdad dashgfdhajdasgdhja dgashjdg gdhj gas ghjdgashgddsdad dashgfdhajdasgdhja dgashjdg gdhj gas ghjdgashgddsdad dashgfdhajdasgdhja dgashjdg gdhj gas ghjdgashgd';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
